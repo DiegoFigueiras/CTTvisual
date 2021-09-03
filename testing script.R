06da20b60397d3245ffa25b5a25d16177e1b64b0
jtkulas <kulasj@montclair.edu>
2021-09-03 17:34:47 -0400

    separate playground script

---
+++ testing script.R
@@ -0,0 +1,12 @@
+
+eq <- function(x){c + ((1-c)*(1/(1+2.71828^(-1.7*(a*(x-b))))))}
+
+a <- 1
+b <- -.5
+c <- 0
+
+curve(eq, from=-4, to=4, lwd = 3)          ## lwd changes figure weight
+
+a <- 3
+b <- .5
+curve(eq, col="red", add=TRUE, lwd=3)
